d6353f1e051a6d8288bcbd8677ffc2589e8fc4f8
Kamau <kamauwamuigaijr@gmail.com>
2024-06-29 10:45:52 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+# server/models.py
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -16,3 +17,20 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+
+
+
+
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+
+# metadata = MetaData()
+# db = SQLAlchemy(metadata=metadata)
+
+# class Pet(db.Model):
+#     __tablename__ = 'pets'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     species = db.Column(db.String)
